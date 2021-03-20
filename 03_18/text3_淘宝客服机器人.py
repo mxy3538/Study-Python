e058bb5a5afc938e06c480b1d4fd9420ad198196
mxy3538 <mxy3538@qq.com>
2021-03-20 21:38:56 +0800

    修改代码

--- 03_18/text3_淘宝客服机器人.py
+++ 03_18/text3_淘宝客服机器人.py
@@ -4,16 +4,16 @@ while True:
     I = input("我:")
 
     if "包邮" in I:
-        AI="亲，江浙沪包邮哦:)"        
+        AI="亲，江浙沪包邮哦！"        
         print("AI:{}".format(AI))
     elif "快递" in I:
-        AI="亲，本店只发EMS哦:)"        
+        AI="亲，本店只发EMS哦！"        
         print("AI:{}".format(AI))
     elif "88" in I:
-        AI = "亲，记得5星好评哦:)"
+        AI = "亲，记得5星好评哦！"
         print("AI:{}".format(AI))
         break
     else:
-        AI="亲，人工客服忙，稍等哦:)"
+        AI="亲，人工客服忙，稍等哦！"
         print("AI:{}".format(AI))
 
