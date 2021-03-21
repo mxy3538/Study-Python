5be376225763a039b3bbec5757f7540ff37a4434
mxy3538 <mxy3538@qq.com>
2021-03-21 23:43:53 +0800

    add

---
+++ pythonProject_03_21/py code/text2_蟒蛇绘制.py
@@ -0,0 +1,18 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/3/21 23:37
+import turtle
+turtle.setup(650, 350, 200, 200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(4):
+    turtle.circle(40, 80)
+    turtle.circle(-40, 80)
+turtle.circle(40, 80/2)
+turtle.fd(40)
+turtle.circle(16, 180)
+turtle.fd(40 * 2/3)
+turtle.done()
