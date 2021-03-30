895ef38e258777de633443ebde3b50ff9c69a53e
mxy3538 <mxy3538@qq.com>
2021-03-30 21:11:26 +0800

    add

---
+++ pythonProject_03_30/py code/text1.py
@@ -0,0 +1,8 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/3/30 21:08
+import turtle as t
+t.pensize(2)
+for i in range(6):
+    t.fd(150)
+    t.left(60)
+t.done()
---
+++ pythonProject_03_30/py code/text2.py
@@ -0,0 +1,8 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/3/30 21:09
+import turtle as t
+t.pensize(2)
+for i in range(9):
+    t.fd(150)
+    t.left(80)
+t.done()
