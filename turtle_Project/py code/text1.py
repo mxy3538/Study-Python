e3d2bce12eb8751f0ae9083cc943c9c3ca407926
mxy3538 <mxy3538@qq.com>
2021-04-07 00:19:19 +0800

    海龟画图：Spider-Man

---
+++ turtle_Project/py code/text1.py
@@ -0,0 +1,266 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/4/6 17:43
+import turtle as t
+t.pencolor('red')
+t.fillcolor('red')
+
+
+t.begin_fill()
+#1
+t.circle(93)
+t.end_fill()
+
+t.penup()
+t.seth(28)
+t.fd(89)
+t.pendown()
+
+t.begin_fill()
+# 2
+t.seth(-35)
+t.fd(108)
+
+# 3
+t.seth(-90)
+t.fd(168)
+
+# 4
+t.seth(-150)
+t.fd(194)
+
+# 5
+t.seth(150)
+t.fd(194)
+
+# 6
+t.seth(90)
+t.fd(168)
+
+# 7
+t.seth(35)
+t.fd(111)
+
+t.end_fill()
+
+
+t.penup()
+t.seth(110)
+t.fd(165)
+t.pendown()
+
+# 8
+t.begin_fill()
+t.seth(-90)
+t.fd(57)
+
+# 9
+t.seth(-24)
+t.fd(37)
+
+# 10
+t.seth(-90)
+t.fd(77)
+
+# 11
+t.seth(-145)
+t.fd(113)
+
+# 12
+t.seth(-90)
+t.fd(170)
+
+# 13
+t.seth(-141)
+t.fd(40)
+
+# 14
+t.seth(90)
+t.fd(194)
+
+# 15
+t.seth(35)
+t.fd(112)
+
+# 16
+t.seth(90)
+t.fd(68)
+
+# 17
+t.seth(159)
+t.fd(32)
+
+# 18
+t.seth(90)
+t.fd(89)
+
+# 19
+t.seth(-33)
+t.fd(34)
+t.end_fill() #结束填充
+
+
+t.penup()
+t.seth(146)
+t.fd(71)
+t.pendown()
+
+# 20
+t.begin_fill() #填充开始
+t.fd(52)
+
+# 21
+t.seth(-90)
+t.fd(174)
+
+# 22
+t.seth(-23)
+t.fd(78)
+
+# 23
+t.seth(-147)
+t.fd(176)
+
+# 24
+t.seth(-90)
+t.fd(247)
+
+# 25
+t.seth(40)
+t.fd(60)
+
+# 26
+t.seth(90)
+t.fd(219)
+
+# 27
+t.seth(33)
+t.fd(138)
+
+# 28
+t.seth(90)
+t.fd(48)
+
+# 29
+t.seth(153)
+t.fd(51)
+
+# 30
+t.seth(90)
+t.fd(115)
+t.end_fill() #结束填充
+
+
+t.penup()
+t.seth(0)
+t.fd(393)
+t.pendown()
+
+
+# 31
+t.begin_fill() #填充开始
+t.seth(-90)
+t.fd(113)
+
+# 32
+t.seth(-153)
+t.fd(50)
+
+# 33
+t.seth(-90)
+t.fd(50)
+
+# 34
+t.seth(-33)
+t.fd(137)
+
+# 35
+t.seth(-90)
+t.fd(219)
+
+# 36
+t.seth(-40)
+t.fd(60)
+
+# 37
+t.seth(90)
+t.fd(247)
+
+# 38
+t.seth(147)
+t.fd(176)
+
+# 39
+t.seth(23)
+t.fd(78)
+
+# 40
+t.seth(90)
+t.fd(174)
+
+# 41
+t.seth(-146)
+t.fd(52)
+t.end_fill() #结束填充
+
+
+t.penup()
+t.seth(-146)
+t.fd(36)
+t.pendown()
+
+# 42
+t.begin_fill() #填充开始
+t.seth(-147)
+t.fd(34)
+
+# 43
+t.seth(-90)
+t.fd(57)
+
+# 44
+t.seth(-156)
+t.fd(37)
+
+# 45
+t.seth(-90)
+t.fd(77)
+
+# 46
+t.seth(-35)
+t.fd(113)
+
+# 47
+t.seth(-90)
+t.fd(170)
+
+# 48
+t.seth(-39)
+t.fd(40)
+
+# 49
+t.seth(90)
+t.fd(194)
+
+# 50
+t.seth(145)
+t.fd(112)
+
+# 51
+t.seth(90)
+t.fd(68)
+
+# 52
+t.seth(21)
+t.fd(32)
+
+# 53
+t.seth(90)
+t.fd(89)
+t.end_fill()
+t.end_fill() #结束填充
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.done()
