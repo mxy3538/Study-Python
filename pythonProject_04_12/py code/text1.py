8adaa6339ee3e269800b7a5aa02f3c5a89ab72ce
mxy3538 <mxy3538@qq.com>
2021-04-12 18:31:09 +0800

    add

---
+++ pythonProject_04_12/py code/text1.py
@@ -0,0 +1,5 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/4/12 17:27
+z=2.3e+3-1.34e-3j
+print("a=",z.real)
+print("b=",z.imag)
---
+++ pythonProject_04_12/py code/text2.py
@@ -0,0 +1,9 @@
+# -*- coding：uft-8 -*-
+# 日期：2021/4/12 18:07
+
+
+a=pow(3,4,5)
+b=abs(-6)
+c=divmod(11,4)
+d=round(3.1415926,3)
+print(a,b,c,d)
